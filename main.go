e6ba6f4cf59d621f01e6774166880f958902ec9a
Ivan <imelekhin@gmail.com>
2020-02-10 21:09:45 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,227 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/tls"
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+type jiraProject struct {
+	Key string `json:"key"`
+}
+
+type jiraIssueType struct {
+	Id string `json:"id"`
+}
+
+type jiraFields struct {
+	Project       jiraProject   `json:"project"`
+	Issuetype     jiraIssueType `json:"issuetype"`
+	Summary       string        `json:"summary"`
+	Description   string        `json:"description"`
+	SrcIP         string        `json:"customfield_10225"`
+	DstIP         string        `json:"customfield_10226"`
+	Organizations [1]int        `json:"customfield_10002"`
+	Priority      jiraPriority  `json:"priority"`
+	Srcname       string        `json:"customfield_10230"`
+	Dstname       string        `json:"customfield_10229"`
+}
+
+type jiraMsg struct {
+	Fields jiraFields `json:"fields"`
+}
+
+type jiraPriority struct {
+	Priority string `json:"id"`
+}
+type kafkaMsg struct {
+	SrcIP        string `json:"srcip"`
+	DstIP        string `json:"dstip"`
+	Message      string `json:"message"`
+	Summary      string `json:"summary"`
+	Description  string `json:"desc"`
+	Timestamp    string `json:"@timestamp"`
+	CustomerID   string `json:"type"`
+	Organisation int    `json:"org"`
+}
+
+type server struct {
+}
+
+var (
+	kafkaURL      = flag.String("kafka-broker", "127.0.0.1:9092", "Kafka broker URL list")
+	intopic       = flag.String("kafka-in-topic", "notopic", "Kafka topic to read from")
+	jiraURL       = flag.String("jira-url", "127.0.0.1", "Jira instance")
+	groupID       = flag.String("kafka-group", "nogroup", "Kafka group")
+	statPort      = flag.String("stat-port", "1234", "Port to bind statistic endpoint")
+	jiraProjectID = flag.String("jira-project", "TI", "Jira project name")
+	user          = flag.String("user", "gw", "user name for Jira")
+	pass          = flag.String("pass", "1234", "Password for Jira")
+	msg           kafkaMsg
+	alert         jiraMsg
+	logger        *log.Logger
+	reader        *kafka.Reader
+)
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	str, _ := json.Marshal(reader.Stats())
+	w.Write([]byte(str))
+}
+
+func getKafkaReader(kafkaURL, topic, groupID string) *kafka.Reader {
+	brokers := strings.Split(kafkaURL, ",")
+	return kafka.NewReader(kafka.ReaderConfig{
+		Brokers:     brokers,
+		GroupID:     groupID,
+		Topic:       topic,
+		MinBytes:    10e3, // 10KB
+		MaxBytes:    10e6, // 10MB
+		MaxWait:     3 * time.Second,
+		StartOffset: kafka.LastOffset,
+		ErrorLogger: logger,
+	})
+}
+
+func convertKafkaJira(alarm kafkaMsg) jiraMsg {
+	var msg jiraMsg
+
+	namesSrc, err := net.LookupAddr(alarm.SrcIP)
+
+	if err != nil {
+		msg.Fields.Srcname = ""
+	} else {
+		msg.Fields.Srcname = strings.Join(namesSrc, " ")
+	}
+
+	namesDst, err := net.LookupAddr(alarm.DstIP)
+	if err != nil {
+		msg.Fields.Dstname = ""
+	} else {
+		msg.Fields.Dstname = strings.Join(namesDst, " ")
+	}
+
+	if len(alarm.CustomerID) == 0 {
+
+		msg.Fields.Project.Key = *jiraProjectID
+	} else {
+		msg.Fields.Project.Key = alarm.CustomerID
+	}
+
+	msg.Fields.Summary = alarm.Summary
+	msg.Fields.Description = alarm.Description
+	msg.Fields.Issuetype.Id = "10007"
+	msg.Fields.SrcIP = alarm.SrcIP
+	msg.Fields.DstIP = alarm.DstIP
+	msg.Fields.Priority.Priority = "4"
+	msg.Fields.Organizations[0] = alarm.Organisation
+	return msg
+}
+
+func init() {
+	flag.Parse()
+	logger = log.New(os.Stdout, "kafkajira:", log.Ldate|log.Ltime)
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func sendJiraAlert() {
+
+	jsonValue, _ := json.Marshal(convertKafkaJira(msg))
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client := &http.Client{
+		Transport: tr,
+	}
+
+	req, err := http.NewRequest("POST", *jiraURL+"/rest/api/2/issue", bytes.NewBuffer(jsonValue))
+
+	req.Header.Add("Authorization", "Basic "+basicAuth(*user, *pass))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		logger.Printf("The HTTP request failed with error %s\n", err)
+	}
+
+	resp.Body.Close()
+
+}
+
+//src ip - 8 hihgest byte, dst ip - 8 lowest byte map to last seen
+
+func main() {
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	reader = getKafkaReader(*kafkaURL, *intopic, *groupID)
+
+	defer func() {
+		reader.Close()
+		close(sigs)
+	}()
+
+	go func() {
+		s := &server{}
+		http.Handle("/metrics", s)
+		logger.Fatal(http.ListenAndServe(":"+*statPort, nil))
+	}()
+
+	logger.Println("start consuming ... !!")
+
+	start := time.Now()
+
+loop:
+	for {
+
+		select {
+		case sig := <-sigs:
+			logger.Println(sig)
+			break loop
+		default:
+			m, err := reader.ReadMessage(context.Background())
+
+			if err != nil {
+				logger.Println(err)
+				break
+			}
+
+			err = json.Unmarshal([]byte(m.Value), &msg)
+
+			if err != nil {
+				logger.Println(err)
+				break
+			}
+
+			sendJiraAlert()
+		}
+	}
+
+	logger.Println("Terminating")
+	elapsed := time.Since(start)
+	logger.Printf("Message processed %d in %s\n", reader.Stats().Messages, elapsed)
+
+}
